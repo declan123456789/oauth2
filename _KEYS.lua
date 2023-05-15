f52bc4824c5e44ae7948d7a26c39f2615d786666
mrlokiie123 <133542179+mrlokiie123@users.noreply.github.com>
2023-05-14 22:33:05 -0400

    Update and rename _KEYS.lua to _KEYS

--- _KEYS.lua
+++
@@ -1,6 +0,0 @@
-local DataStore = game:GetService('DataStoreService');
-local OneMonthKey = DataStore:GetDataStore('OneMonthKey')
-
-local Keys = {
-    ['MrLokiies_KEY'] = "XB3491X0NVB.BNX1-%DS,SBXM",
-};
